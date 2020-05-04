1f2e5a088c7a23065911d4d56fe656a4f2544bbb
Narumi Nogawa <naruo0711@ab.auone-net.jp>
2020-05-04 11:45:49 +0900

    タイマー関数の実装

--- swiftNodeConnectionPractice/ViewController.swift
+++ swiftNodeConnectionPractice/ViewController.swift
@@ -11,15 +11,19 @@ import Alamofire
 import SwiftyJSON
 
 class ViewController: UIViewController {
+    @IBOutlet weak var connectButton: UIButton!
+    
     let url = "http://localhost:7070"
     let url2 = "http://0.0.0.0:7070"
+    
+    let timer = DispatchSource.makeTimerSource(flags: [], queue: .main)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        //繰り返し処理をメインスレッドにて行う
-        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.5) {
+        self.timer.schedule(deadline: .now(), repeating: 0.5)
+        self.timer.setEventHandler {
             //urlからjsonを取得する
             Alamofire.request(self.url2)
                 .responseJSON { response in
@@ -27,12 +31,30 @@ class ViewController: UIViewController {
                         return
                     }
                     let json = JSON(object)
-                    let platform = json["platform"].string!
-                    let nodeVersion = json["nodeVersion"].string!
-                    let uptime = json["uptime"].string!
+                    //let platform = json["platform"].string
+                    //let nodeVersion = json["nodeVersion"].string
+                    //let uptime = json["uptime"].string
                     print(json)
             }
         }
     }
+    
+    @IBAction func connectButtonTapped(_ sender: Any) {
+        // 開始
+        timer.resume()
+        // 一時停止
+        //timer.suspend()
+        //タイマー破棄
+        //timer.cancel()
+    }
+    
+}
+
+//グローバルな関数として定義
+//遅延実行の関数
+func delay(_ delay: Double, closure: @escaping ()->()) {
+  DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
+    closure()
+  }
 }
 
