431b41068fef5f4876ad8a4bbb5ced86270af409
Narumi Nogawa <naruo0711@ab.auone-net.jp>
2020-05-05 10:21:53 +0900

    画面遷移と遷移先のUI設計

--- swiftNodeConnectionPractice/AjaxViewController.swift
+++ swiftNodeConnectionPractice/AjaxViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import Alamofire
 import SwiftyJSON
 
-class ViewController: UIViewController {
+class AjaxViewController: UIViewController {
     @IBOutlet weak var connectButton: UIButton!
     @IBOutlet weak var platformLabel: UILabel!
     @IBOutlet weak var nodeVersionLabel: UILabel!
---
+++ swiftNodeConnectionPractice/WebSocketViewController.swift
@@ -0,0 +1,39 @@
+//
+//  WebSocketViewController.swift
+//  swiftNodeConnectionPractice
+//
+//  Created by Narumi Nogawa on 2020/05/05.
+//  Copyright © 2020 Narumi Nogawa. All rights reserved.
+//
+
+import UIKit
+
+class WebSocketViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return cell
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
